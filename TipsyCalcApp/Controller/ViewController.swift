313e63dcd3ab7446ccf95994a442463f4c147994
Nicolas TERRONE <nicolasterrone@gmail.com>
2019-01-11 21:35:37 +0100

    Constraints upgrade, Outlet and Action created.

--- TipsyCalcApp/Controller/ViewController.swift
+++ TipsyCalcApp/Controller/ViewController.swift
@@ -10,11 +10,21 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var textField: UITextField!
+    @IBOutlet weak var tipPercentageLbl: UILabel!
+    @IBOutlet weak var tipPercentageSlider: UISlider!
+    @IBOutlet weak var tipLbl: UILabel!
+    @IBOutlet weak var totalLbl: UILabel!
+    
+    var tip = TipModel(billAmount: 0.0, tipPercent: 0.0)
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
 
-
+    @IBAction func billAmountDidChange(_ sender: Any) {
+        print("My value changed!")
+    }
+    
 }
 
